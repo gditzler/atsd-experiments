7d4be73728213d6869f8753f91d86405b0530bba
Gregory Ditzler <ditzler@email.arizona.edu>
2016-05-12 17:37:27 -0700

    init commit

---
+++ src/atsd/svm_search_matlab.m
@@ -0,0 +1,43 @@
+function [x, f] =svm_search_matlab(dataset)
+
+n_runs = 5;
+
+X = load(dataset);   % DATASET is a global var in the main experimenter
+
+% load the data and split
+data = X(:, 1:end-1); 
+labels  = X(:, end);  % labels are in the last dimension 
+if length(unique(labels)) ~= 2
+  error('Must be a two class problem.');
+end
+data = zscore(data);
+
+[ns, ~] = size(data);
+q = randperm(ns);
+
+data = data(q, :);
+labels = labels(q);
+labels(labels == 0) = -1;
+
+c = cvpartition(numel(labels), 'KFold', 10);
+minfn = @(z)kfoldLoss(fitcsvm(data, labels, ...
+                              'CVPartition', c , ...
+                              'KernelFunction', 'rbf', ...
+                              'BoxConstraint', exp(z(2)), ...
+                              'KernelScale',exp(z(1))));
+                            
+opts = optimset('TolX', 5e-4, 'TolFun', 5e-4);
+
+fval = zeros(n_runs, 1);
+z = zeros(n_runs, 2);
+parfor j = 1:n_runs
+    [searchmin, fval(j)] = fminsearch(minfn, randn(2,1), opts);
+    z(j,:) = exp(searchmin);
+end
+z = z(fval == min(fval),:);
+
+
+% C, \sigma
+x = [z(2);z(1)];
+f = min(fval);
+
