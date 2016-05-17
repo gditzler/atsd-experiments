98940d31632a617f14e751f78dd2f844f46d325a
Gregory Ditzler <ditzler@email.arizona.edu>
2016-05-17 10:55:45 -0700

    updated with matlabs optimizer

--- src/run_cv_summaries.m
+++ src/run_cv_summaries.m
@@ -44,22 +44,21 @@ all_datas_2 = {
   'vertebral-column-2clases'};
 
 n_data = length(all_datas_2);
-all_parameters = cell(n_data, 4);
-all_errors = zeros(n_data, 4);
+all_parameters = cell(n_data, 5);
+all_errors = zeros(n_data, 5);
 
 delete(gcp('nocreate'));
 parpool(4);
 
 % loop over all of the data sets
 for nn = 1:n_data
-  
+  data = load(['~/Git/ClassificationDatasets/csv/', all_datas_2{nn}, '.csv']);
   str = [all_datas_2{nn}, ' & '];
   
   for ee = 1:4
     % load the result file that contains the parameters of the classifier
     % then load the data set 
     load(['outputs/result_', all_datas_2{nn}, '_moo_exp0', num2str(ee), '.mat']);
-    data = load(['~/Git/ClassificationDatasets/csv/', all_datas_2{nn}, '.csv']);
     
     % find the number of parameters that were on the pareto front and
     % search for the parameters that give use the smallest cross validation
@@ -82,6 +81,13 @@ for nn = 1:n_data
     all_errors(nn, ee) = err;
     all_parameters{nn, ee} = min_param;
   end
+  
+  load(['outputs/result_', all_datas_2{nn}, '_matlab.mat']);
+  current_err = run_cv(data, x(1), x(2));
+  str = [str, num2str(current_err), ' & '];
+  all_errors(nn, 5) = current_err;
+  all_parameters{nn, 5} = x;
+    
   str = [str(1:end-2), '\\'];
   disp(str);
   
