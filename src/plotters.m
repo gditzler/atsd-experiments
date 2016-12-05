ce0e9a6dccc1e4b8d164a3c3f7f2b9720ddc3309
Gregory Ditzler <gregory.ditzler@gmail.com>
2016-12-05 12:02:31 -0700

    updated plot

--- src/plotters.m
+++ src/plotters.m
@@ -3,35 +3,35 @@ clc
 clear
 close all
 
-x = -2:0.1:2;
+x = -2:0.25:2;
 %br = x.^2;
 er = 7*exp(-2*(x+2))+.5;
 ci = fliplr(er);
 br = er+ci;
-lw = 1;
-ms = 10;
+lw = 3;
+ms = 20;
 
 h = figure;
 hold on;
 box on;
-plot(x, br, '-s', 'LineWidth', lw, 'MarkerSize', ms);
-plot(x, er, '-o', 'LineWidth', lw, 'MarkerSize', ms);
-plot(x, ci, '-p', 'LineWidth', lw, 'MarkerSize', ms);
-plot([-1 -1], [-10 2], 'k--')
-plot([0 0], [-10 min(br)], 'k')
-plot([1 1], [-10 2], 'k--')
-text(-.95, .19, '$$h_1$$','Interpreter','latex', 'fontsize', 30)
-text(0.05, .19, '$$h^*$$','Interpreter','latex', 'fontsize', 30)
-text(1.05, .19, '$$h_2$$','Interpreter','latex', 'fontsize', 30)
-xlabel('$$h$$','Interpreter','latex', 'FontSize', 30)
+plot(x, br, 'k-s', 'LineWidth', lw, 'MarkerSize', ms);
+plot(x, er, 'r-o', 'LineWidth', lw, 'MarkerSize', ms);
+plot(x, ci, 'b-o', 'LineWidth', lw, 'MarkerSize', ms);
+plot([-1 -1], [-10 2], '--', 'Color', [.5 .5 .5],'LineWidth', 1+lw)
+plot([0 0], [-10 min(br)], '--', 'Color', [.5 .5 .5],'LineWidth', 1+lw)
+plot([1 1], [-10 2], '--', 'Color', [.5 .5 .5],'LineWidth', 1+lw)
+text(-.95, .19, '$$h_1$$','Interpreter','latex', 'fontsize', 35)
+text(0.05, .19, '$$h^*$$','Interpreter','latex', 'fontsize', 35)
+text(1.05, .19, '$$h_2$$','Interpreter','latex', 'fontsize', 35)
+xlabel('$$h$$','Interpreter','latex', 'FontSize', 35)
 ylim([0, 4])
-
-legend('Bounded Risk', 'Empirical Risk', 'Confidence Interval', 'Location', 'best')
 set(gca,'XTickLabel','')
 set(gca,'YTickLabel','')
-set(gca, 'fontsize', 20)
+set(gca, 'fontsize', 25)
+
+legend('Bounded Risk', 'Empirical Risk', 'Confidence Interval', 'Location', 'north')
 saveas(h, 'plots/risk-plot.fig')
-saveas(h, 'plots/eps/risk-plot.eps', 'eps2c')
+saveas(h, 'plots/risk-plot.eps', 'eps2c')
 %% result tables 
 clc
 clear
